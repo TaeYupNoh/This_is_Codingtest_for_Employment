0e58353eae1aa56d731257c275956b76723ba9ab
TaeYupNoh <107118671+TaeYupNoh@users.noreply.github.com>
2022-07-11 00:36:25 +0900

    until_to_be_1.py

--- until_to_be_1.py
+++ until_to_be_1.py
@@ -1,14 +1,28 @@
 # 1이 될 때까지 p.99
 n, k = map(int, input().split())
-
 cnt = 0
-while n != 1:
-    if n % k != 0:
-        n -= 1
-        cnt += 1
 
-    else:
-        n //= k
-        cnt += 1
+while True:
+    well_did = (n//k)*k
+    cnt += (n-well_did)
+    n = well_did
+    if n < k:
+        break
+    cnt += 1
+    n //= k
 
+cnt += (n-1)
 print(cnt)
+
+# 숫자가 커지면 1씩 계속 빼는 것 비효율적
+# cnt = 0
+# while n != 1:
+#     if n % k != 0:
+#         n -= 1
+#         cnt += 1
+
+#     else:
+#         n //= k
+#         cnt += 1
+
+# print(cnt)
