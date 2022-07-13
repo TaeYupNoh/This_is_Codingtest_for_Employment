2a6d76f3153554bd5590edb910511e0623cedb37
TaeYupNoh <107118671+TaeYupNoh@users.noreply.github.com>
2022-07-13 16:18:43 +0900

    예제 4-1 상하좌우

---
+++ udlr.py
@@ -0,0 +1,27 @@
+n=int(input())
+move_lis=input().split()
+def outchecker():
+    global a,b,n
+    if a <1 :
+        a=1
+    if a>n :
+        a=n
+    if b<1 :
+        b=1
+    if b>n :
+        b=n
+
+a,b = 1,1
+for i in move_lis:
+    if i == 'R':
+        b+=1
+    elif i == 'L':
+        b-=1
+    elif i == 'U':
+        a-=1
+    elif i == 'D':
+        a+=1
+
+    outchecker()
+
+print(a,b)
